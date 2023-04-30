a18454c58f69d2e2bf7efd7fb4808d95036144e8
AdamKav-student <123458911+AdamKav-student@users.noreply.github.com>
2023-04-30 23:15:55 +0100

    Nav-Mesh
    New Nav Mesh for the AI which is used for an NPC which will be chased by the others. As current AI will magnetise towards the AI designated as chaser

--- Respond/Assets/AdamFolder/Adam_Scripts/Npc_NavMesh.cs
+++ Respond/Assets/AdamFolder/Adam_Scripts/Npc_NavMesh.cs
@@ -7,40 +7,37 @@ public class Npc_NavMesh : MonoBehaviour
 
 {
 public enum AIState {
-    Idle,Roam,Following
+    Roam,Chase
 }
     private NavMeshAgent _navMeshAgent;
-    private GameObject Tagged;
-    public AIState currentState = AIState.Idle;
+    private GameObject Runner;
+    public AIState currentState = AIState.Roam;
 
     void Start() {
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        Tagged = GameObject.FindWithTag("Tag");
+        Runner = GameObject.FindWithTag("Tagged");
     }
 
     void Update() {
 
            switch (currentState) {
-      case AIState.Idle:
-         // do nothing
-         break;
-      case AIState.Roam:
+              case AIState.Roam:
          // move to a random location on the NavMesh
          if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.1f) {
             SetRandomDestination();
          }
          break;
-        case AIState.Following:
-        _navMeshAgent.SetDestination(Tagged.transform.position);
+        case AIState.Chase:
+        _navMeshAgent.SetDestination(Runner.transform.position);
         break; 
            }     
    
- float distanceToPlayer = Vector3.Distance(transform.position, Tagged.transform.position);
-   if ((currentState == AIState.Idle || currentState == AIState.Roam) && distanceToPlayer < 5f) {
+ float distanceToPlayer = Vector3.Distance(transform.position, Runner.transform.position);
+   if ((currentState == AIState.Chase || currentState == AIState.Roam) && distanceToPlayer < 5f) {
       {
-         currentState = AIState.Following;
+         currentState = AIState.Chase;
       }
-      if ((currentState == AIState.Following) && distanceToPlayer > 5f) {
+      if ((currentState == AIState.Chase) && distanceToPlayer > 5f) {
          {
             currentState = AIState.Roam;
          } 
